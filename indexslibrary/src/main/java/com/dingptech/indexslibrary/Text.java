05ceaa9b2d5317b9cea2936170e1fd396e92a21d
MicroWin10-1420\Administrator <1059826667@qq.com>
2019-05-09 18:18:46 +0800

    no message

---
+++ indexslibrary/src/main/java/com/dingptech/indexslibrary/Text.java
@@ -0,0 +1,26 @@
+package com.dingptech.indexslibrary;
+
+/*
+         |              |
+         | \            | \
+         |   | | | | | |    | | | | |||||\
+         |                          |||||||\
+         |         ( )              ||||||||
+         |                           |||||/
+         |                  | | | | | |||/
+         |    |             |          |
+         |    |             |          |
+       / |   | |            |          |\
+      |      |/             |          \|
+       \ |                  |
+         |                  |
+           \ | | | | | | | /
+             |       |            <-----弱鸡
+             |       |
+             |       |
+*/
+public class Text {
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+}
